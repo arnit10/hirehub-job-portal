d9526e1c0d8720fe0ab068c125f3d2a3942a17bd
arnit10 <arnitchauhan10@gmail.com>
2025-09-16 10:53:38 +0530

    Implement signup, login, and profile creation views with templates

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("candidate/signup", candidate_signup, name = "candidate_signup")
+    path("", home_view, name="home"),
+    path("signup/", user_signup, name = "user_signup"),
+    path("login/", UserLoginView.as_view(), name= "user_login" ),
+    path("logout/", user_logout_view, name="user_logout"),
+    path("candidate-profile", candidate_profile_create, name= "candidate_profile_create"),
+    path("recruiter-profile", recruiter_profile_create, name= "recruiter_profile_create"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,38 +1,82 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from .models import *
+from .forms import *
+
 
 # Create your views here.
-def candidate_signup(request):
+#homepage view
+def home_view(request):
+    return render(request, "users/home.html")
+
+#user login view
+class UserLoginView(LoginView):
+    template_name = "users/login.html"
+    authentication_form = CustomLoginForm
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        user = self.request.user
+
+        if user.role == "C":
+            return reverse_lazy("candidate_dashboard")
+
+        elif user.role == "R":
+            return reverse_lazy("recruiter_dashboard")
+
+        return reverse_lazy("home")
+
+#user logout view
+def user_logout_view(request):
+    logout(request)
+    return redirect("home")
+
+#user signup view
+def user_signup(request):
     if request.method == "POST":
-        # User Fields
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
+        form = UserSignUpForm(request.POST)
 
-        #Candidate Fields
-        full_name = request.POST['full_name']
-        phone_number = request.POST['phone_number']
-        resume = request.FILES.get('resume')
-        skills = request.POST['skills']
-        experience = request.POST['experience']
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
 
-        #user creation
-        user = User.objects.create_user(
-            username = username,
-            email = email,
-            password = password,
-            role = 'J'
-        )
+            if user.role == "J":
+                return redirect("candidate_profile_create")
+            elif user.role == "R":
+                return redirect("recruiter_profile_create")
 
-        #candidate creation
-        CandidateProfile.objects.create(
-            user = user,
-            full_name = full_name,
-            phone_number = phone_number,
-            resume = resume,
-            skills = skills,
-            experience = experience
-        )
+    else:
+        form = UserSignUpForm()
+    return render(request, "users/signup.html", {'form': form} )
 
-        return redirect("login")
-    return render(request, "users/candidate_signup.html")
+#job seeker profile creation view
+@login_required
+def candidate_profile_create(request):
+    if request.method == "POST":
+        form = CandidateSignUpForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit= False)
+            profile.user = request.user
+            profile.save()
+            return redirect('dashboard')
+    else:
+        form = CandidateSignUpForm()
+
+    return render(request, 'users/candidateprofileform.html', {'form': form})
+
+#recruiter profile creation view
+@login_required
+def recruiter_profile_create(request):
+    if request.method == "POST":
+        form = RecruiterSignUpForm(request.POST)
+        if form.is_valid():
+            profile = form.save(commit= False)
+            profile.user = request.user
+            profile.save()
+            return redirect('dashboard')
+    else:
+        form = RecruiterSignUpForm()
+    return render(request, 'users/recruiterprofileform.html', {'form': form}) 
